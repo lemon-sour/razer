26737ce73758b453b4df8d87e945f393cbeb9452
hisasann <hisasann25@gmail.com>
2019-02-10 17:05:14 +0900

    feat(index): prod and production mode does't show console.log message

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,15 +1,44 @@
 import razer from '../src/index';
+import C from '../src/common/constants';
 
-describe('index', () => {
-  test('test single args', () => {
-    razer('test single args');
-
-    expect(1).toEqual(1);
+describe('index test', () => {
+  test('test single arg', () => {
+    expect.assertions(1);
+    const b = razer('test single arg');
+    expect(b).toEqual(true);
   });
 
   test('test multi args', () => {
-    razer('test', 'multi', 'args', 1, true);
+    expect.assertions(1);
+    const b = razer('test', 'multi', 'args', 1, true);
+    expect(b).toEqual(true);
+  });
 
-    expect(1).toEqual(1);
+  test('should not show message when test is ' + C.PROD, () => {
+    expect.assertions(1);
+    process.env.NODE_ENV = C.PROD;
+    const b = razer(C.PROD);
+    expect(b).toEqual(false);
+  });
+
+  test('should not show message when test is ' + C.PRODUCTION, () => {
+    expect.assertions(1);
+    process.env.NODE_ENV = C.PRODUCTION;
+    const b = razer(C.PRODUCTION);
+    expect(b).toEqual(false);
+  });
+
+  test('should show message when test is stg', () => {
+    expect.assertions(1);
+    process.env.NODE_ENV = 'stg';
+    const b = razer('stg');
+    expect(b).toEqual(true);
+  });
+
+  test('should show message when test is dev', () => {
+    expect.assertions(1);
+    process.env.NODE_ENV = 'dev';
+    const b = razer('dev');
+    expect(b).toEqual(true);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export default function razer(...args: any[]): void {
+import C from './common/constants';
+
+export default function razer(...args: any[]): boolean {
+  if (!args) {
+    return false;
+  }
+
+  if (args.length <= 0) {
+    return false;
+  }
+
+  // prod 版のビルドのときは console.log を無効化する
+  if (process.env.NODE_ENV === C.PROD || process.env.NODE_ENV === C.PRODUCTION) {
+    return false;
+  }
+
   console.log(...args);
+  return true;
 }
